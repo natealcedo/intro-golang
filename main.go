21ca173753d9b94acbc0be72b8f5d8bfe31d2855
Nathaniel Alcedo <nate@natealcedo.dev>
2024-07-23 15:42:10 +0800

    Go over pointers

--- main.go
+++ main.go
@@ -2,34 +2,23 @@ package main
 
 import "fmt"
 
-type CustomMap[K comparable, V any] struct {
-	data map[K]V
+type Database struct {
+	user string
 }
 
-func NewCustomMap[K comparable, V any]() *CustomMap[K, V] {
-	return &CustomMap[K, V]{data: make(map[K]V)}
+type Server struct {
+	db *Database
 }
 
-func (m *CustomMap[K, V]) Insert(k K, v V) error {
-	m.data[k] = v
-	return nil
-}
-
-func foo[T any](val T) {
-	fmt.Println(val)
+func (s *Server) GetUserFromDB() string {
+	if s.db == nil {
+		panic("Database is not initialized")
+	}
+	return s.db.user
 }
 
 func main() {
-	//m1 := NewCustomMap[string, int]()
-	//m1.Insert("foo", 1)
-	//m1.Insert("bar", 2)
-	//
-	//m2 := NewCustomMap[int, float64]()
-	//m2.Insert(1, 9.7)
-	//m2.Insert(5, 9.7)
-	//
-	//fmt.Printf("m1 := %+v\n", m1)
-	//fmt.Printf("m2 := %+v\n", m2)
-
-	foo("hello world")
+	s := &Server{}
+	user := s.GetUserFromDB()
+	fmt.Println("users ", user)
 }
